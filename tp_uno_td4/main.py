1d18c4b5a70443fb595b94a4bcc2d095a1204bf2
Juan Ignacio Elosegui <112285805+juanielosegui@users.noreply.github.com>
2024-05-23 21:54:29 -0300

    Add files via upload

---
+++ tp_uno_td4/main.py
@@ -0,0 +1,6 @@
+# Bautista Lobo, Massimo Giammaria y Juan Ignacio Elosegui
+# TD IV: Redes de Computadoras
+
+from auxiliares import *
+
+nombre_dominio:str = input("Ingresá el nombre del dominio a resolver: ")
