f952fe49852161b736b4b2a65dcb8c14933f6abd
Juan Ignacio Elosegui <112285805+juanielosegui@users.noreply.github.com>
2024-05-24 20:18:37 -0300

    Delete tp_uno_td4 directory

--- tp_uno_td4/main.py
+++
@@ -1,6 +0,0 @@
-# Bautista Lobo, Massimo Giammaria y Juan Ignacio Elosegui
-# TD IV: Redes de Computadoras
-
-from auxiliares import *
-
-nombre_dominio:str = input("Ingresá el nombre del dominio a resolver: ")
